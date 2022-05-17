7aa305d7d229cebb3b4044d6d6c022496fbd1a2a
abdullah dağlı <abdullah.dagli@outlook.com>
2022-05-17 23:35:44 +0300

    17 Mayıs Ders Sonu

---
+++ Hafta3_Odev1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+
+class Program
+{
+
+    static void Main()
+    {/*
+        Değer türleri(int, double, float, char .vb) belleğin stack bölgesinde tutulur.
+
+        Referans türleri (string, diziler, object, nesneler) belleğin heap bölgesinde tutulur.
+
+        Değer türleri referans türlerine göre daha hızlı çalışır ancak belleğin stack alanı sınırlı olduğu için diziler, 
+        nesneler belleğin heap bölgesinde oluşturulur.
+
+        Heap bölgesinde oluşturulan referans türlerine belleğin başlangıç adresi ile erişim sağlanır ve herhangi bir yerde
+        yapılan değişiklik verinin değişmesine neden olur.
+
+        Ancak ref ile kullanımda değişkene başlangıç değerinin verilmesi gerekirken out anahtar kelimesi ile kullanımda başlangıç 
+        değerinin verilmesine gerek yoktur.
+
+      * */
+
+        int sayimizOut;
+        DegistirOut(out sayimizOut);
+        Console.WriteLine(sayimizOut);
+
+        int sayimizRef = 0;  // değer verilmediğinde hata verecektir.
+        DegistirRef(ref sayimizRef);
+        Console.WriteLine(sayimizRef);
+    }
+
+    static void DegistirRef(ref int sayiRef)
+    {
+        sayiRef = 1234;
+    }
+
+    static void DegistirOut(out int sayiOut)
+    {
+        sayiOut = 1234;
+    }
+}
