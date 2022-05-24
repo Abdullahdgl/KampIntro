57f2e4c494503e44675191ac9bc7920943586058
abdullah dağlı <abdullah.dagli@outlook.com>
2022-05-24 05:31:52 +0300

    24 Mayıs

---
+++ AbstractDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AbstractDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AMevzuat aMevzuat = new AMevzuat();
+			aMevzuat.Degerlendir();
+			aMevzuat.Kaydet();
+			Console.ReadLine();
+	
+        }
+	}
+
+
+	abstract class MevzuatBase
+	{
+		public abstract void Degerlendir();
+		public void Kaydet()
+		{
+			Console.WriteLine("Kaydedildi.");
+		}
+	}
+
+	class AMevzuat : MevzuatBase
+	{
+		public override void Degerlendir()
+		{
+			Console.WriteLine("A Mevzuatına göre değerlendirildi.");
+		}
+	}
+	class BMevzuat : MevzuatBase
+	{
+		public override void Degerlendir()
+		{
+			Console.WriteLine("B Mevzuatına göre değerlendirildi.");
+		}
+	}
+	class CustomerManager
+	{
+		public void Add()
+		{
+			Console.WriteLine("Eklenmiştir.");
+		}
+		public void Delete()
+		{
+			Console.WriteLine("Silinmiştir.");
+		}
+	}
+
+
+}
